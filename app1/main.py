bc8db28a3579e0a7f4b086c553a9c628b511ba0d
Plezi Devops <plaurentai@gmail.com>
2024-06-05 19:32:59 -0400

    use with statement for file and writing the file

---
+++ app1/main.py
@@ -0,0 +1,38 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input('Enter a todo: ') + '\n'
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todos.append(todo)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            file = open('todos.txt', 'w')
+
+        case 'show':
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            for index, item in enumerate(todos):
+                item = item.strip('\n')
+                row = f'{index + 1}-{item}'
+                print(row)
+
+        case 'edit':
+            number = int(input('Number of the todo to edit: '))
+            number -= 1
+            new_todo = input('Enter new todo: ')
+            todos[number] = new_todo
+
+        case 'complete':
+            number = int(input('Number of the todo to complete'))
+            todos.pop(number - 1)
+        case 'exit':
+            exit()
+print('Bye!')
