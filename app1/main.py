1c622775e61ef420a7bb421739f93957c994c128
Plezi Devops <plaurentai@gmail.com>
2024-06-06 08:34:42 -0400

    work on complete option

--- app1/main.py
+++ app1/main.py
@@ -13,7 +13,6 @@ while True:
 
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
-            file = open('todos.txt', 'w')
 
         case 'show':
             with open('todos.txt', 'r') as file:
@@ -27,12 +26,32 @@ while True:
         case 'edit':
             number = int(input('Number of the todo to edit: '))
             number -= 1
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input('Enter new todo: ')
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'complete':
             number = int(input('Number of the todo to complete'))
-            todos.pop(number - 1)
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index]
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f'Todo {todo_to_remove.strip()} was removed from the list'
+            print(message)
+
         case 'exit':
             exit()
 print('Bye!')
